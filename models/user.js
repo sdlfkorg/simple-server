e22bab11a0beaaa9f18afc380f732c070c683ab9
sdlfkorg <sdlfkorg@gmail.com>
2018-07-06 15:46:47 +0800

    add and define user model

---
+++ models/user.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// define user model
+const userSchema = new Schema({
+	email: {type: String, unique: true, lowercase: true},
+	password: String
+});
+
+// create model class
+const ModelClass = mongoose.model('user', userSchema);
+
+
+// export the model
+module.exports = ModelClass;
