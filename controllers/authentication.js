82c6d999ffce58a46cad5e49bda773840b4fb4b6
sdlfkorg <sdlfkorg@gmail.com>
2018-07-09 13:03:12 +0800

    setup jwt for user

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,4 +1,12 @@
+const jwt = require('jwt-simple');
 const User = require('../models/user');
+const config = require('../config');
+
+
+function tokenForUser(user){
+	const timestamp = new Date().getTime();
+	return jwt.encode({sub: user.id, iat: timestamp}, config.secret)
+}
 
 exports.signup = function(req, res, next){
 	const email = req.body.email;
@@ -33,7 +41,7 @@ exports.signup = function(req, res, next){
 			}
 
 			// respond to request indicating the user was created
-			res.json({success: true});
+			res.json({token: tokenForUser(user)});
 		});
 
 		
