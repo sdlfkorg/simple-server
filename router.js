19942219ac40b5ce916cce028c7feba2807bd565
sdlfkorg <sdlfkorg@gmail.com>
2018-07-09 15:48:14 +0800

    router: setup passport middleware

--- router.js
+++ router.js
@@ -1,9 +1,16 @@
 const Authentication = require('./controllers/authentication');
+const passportService = require('./services/passport');
+const passport = require('passport');
+
+const requireAuth = passport.authenticate('jwt', {session: false});
 
 module.exports = function(app){
 	// app.get('/', function(req, res, next){
 	// 	res.send(['test', 'node', 'server']);
 	// })
+	app.get('/', requireAuth,function(req, res, next){
+		res.send({hi: 'there'});
+	})
 	app.post('/signup', Authentication.signup)
 
 }
